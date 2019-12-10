5abc3b83b2fd00b25c1f22bd54392a7ef07e590f
Matija Katadžić <matija.katadzic@gmail.com>
2019-12-10 10:46:34 +0100

    vip

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -8,14 +9,27 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.ML;
+using WebApp.ML;
+using WebApp.ML.DataModels;
 
 namespace WebApp
 {
     public class Startup
     {
+        private readonly string _tensorFlowModelFilePath;
+        private readonly string _mlnetModelFilePath;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
+            _tensorFlowModelFilePath = GetAbsolutePath(Configuration["MLModel:TensorFlowModelFilePath"]);
+            _mlnetModelFilePath = GetAbsolutePath(Configuration["MLModel:MLNETModelFilePath"]);
+
+            TensorFlowModelConfigurator tensorFlowModelConfigurator = new TensorFlowModelConfigurator(_tensorFlowModelFilePath);
+
+            // Save the ML.NET model .zip file based on the TensorFlow model and related configuration
+            tensorFlowModelConfigurator.SaveMLNetModel(_mlnetModelFilePath);
         }
 
         public IConfiguration Configuration { get; }
@@ -24,6 +38,9 @@ namespace WebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddPredictionEnginePool<ImageInputData, ImageLabelPredictions>()
+                    .FromFile(_mlnetModelFilePath);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +69,15 @@ namespace WebApp
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        public static string GetAbsolutePath(string relativePath)
+        {
+            FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
+            string assemblyFolderPath = _dataRoot.Directory.FullName;
+
+            string fullPath = Path.Combine(assemblyFolderPath, relativePath);
+            return fullPath;
         }
     }
 }
